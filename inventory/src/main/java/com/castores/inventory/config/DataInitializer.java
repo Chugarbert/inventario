b561684584aec5d5086bb927e52878f2190df526
Leonardo <leonardo_72-90@hotmail.com>
2024-07-08 12:49:03 -0600

    se modifican los permisos de los usuarios

--- inventory/src/main/java/com/castores/inventory/config/DataInitializer.java
+++ inventory/src/main/java/com/castores/inventory/config/DataInitializer.java
@@ -41,11 +41,13 @@ public class DataInitializer {
         return args -> {
             // Create permissions if they do not exist
             String[] permissionNames = {
+                    "Iniciar session",
                     "Ver modulo inventario",
                     "Agregar nuevos productos",
                     "Aumentar inventario",
-                    "Dar de baja/reactivar un producto",
-                    "Ver modulo para Salida de productos",
+                    "Dar de baja un producto",
+                    "Reactivar un producto",
+                    "Ver modulo salida de productos",
                     "Sacar inventario del almacen",
                     "Ver modulo del historico"
             };
@@ -75,17 +77,37 @@ public class DataInitializer {
                 roleRepository.save(almacenistaRole);
             }
 
+            Role guyRole = roleRepository.findByName("guyRole");
+            if (guyRole == null) {
+                guyRole = new Role();
+                guyRole.setName("guyRole");
+                roleRepository.save(guyRole);
+            }
+/* 
+            "Iniciar session",
+            "Ver modulo inventario",
+            "Agregar nuevos productos",
+            "Aumentar inventario",
+            "Dar de baja un producto",
+            "Reactivar un producto",
+            "Ver modulo salida de productos",
+            "Sacar inventario del almacen",
+            "Ver modulo del historico"
+             */
             // Assign permissions to roles
             Set<String> adminPermissionNames = Set.of(
+                    "Iniciar session",
+                    "Ver modulo del historico",
                     "Ver modulo inventario",
                     "Agregar nuevos productos",
-                    "Aumentar inventario",
-                    "Dar de baja/reactivar un producto",
-                    "Ver modulo del historico");
+                    "Dar de baja un producto",
+                    "Reactivar un producto",
+                    "Aumentar inventario");
 
             Set<String> almacenistaPermissionNames = Set.of(
+                    "Iniciar session",
                     "Ver modulo inventario",
-                    "Ver modulo para Salida de productos",
+                    "Ver modulo para salida de productos",
                     "Sacar inventario del almacen");
 
             assignPermissionsToRole(adminRole, adminPermissionNames, permissions);
@@ -108,6 +130,16 @@ public class DataInitializer {
                 user1.setPassword(passwordEncoder.encode("user1password"));
                 user1.setEstatus(1);
                 user1.setRole(almacenistaRole);
+                userRepository.save(user1);
+            }
+
+            if (userRepository.findByCorreo("user2@example.com") == null) {
+                User user1 = new User();
+                user1.setNombre("User2");
+                user1.setCorreo("user2@example.com");
+                user1.setPassword(passwordEncoder.encode("user2password"));
+                user1.setEstatus(1);
+                user1.setRole(guyRole);
                 userRepository.save(user1);
             }
 
